c0a52f88482b77780eb56b15008b2ce5adb6694a
zabido <zabido@gmail.com>
2017-11-20 23:17:54 +0100

    zabido25 pagekite
    pagekite address has been added. Only to show one snapshot waiting for a key.

--- ip_webcam.py
+++ ip_webcam.py
@@ -2,11 +2,16 @@ import urllib
 import cv2
 import numpy as np
 
-url="http://192.168.0.122:4747/cam/1/frame.jpg"
+# This url is to work on ZabiMon nw
+#url="http://192.168.0.122:4747/cam/1/frame.jpg"
 
-while True:
-	imgRes=urllib.urlopen(url)
-	imgNp=np.array(bytearray(imgRes.read()),dtype=np.uint8)
-	img=cv2.imdecode(imgNp,-1)
-	cv2.imshow("IP Webcam",img)
-	cv2.waitKey(10)
+# Url to work on EWA/Int nw
+#url="http://10.149.157.216:4747/cam/1/frame.jpg"
+
+url="http://zabido25.pagekite.me/cam/1/frame.jpg"
+
+imgRes=urllib.urlopen(url)
+imgNp=np.array(bytearray(imgRes.read()),dtype=np.uint8)
+img=cv2.imdecode(imgNp,-1)
+cv2.imshow("IP Webcam",img)
+cv2.waitKey(0)
